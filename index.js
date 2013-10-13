25fb6537cabcb89c38ff579bf54ee1dd0b7b1f06
Andrew Theken <theken.1@osu.edu>
2013-10-13 08:46:00 -0400

    Added lookups for locations.

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ var express = require('express');
 require.extensions['.geojson'] = require.extensions['.json'];
 
 var locations = require('./open-data/flu-shot/city-of-philadelphia/locations.geojson');
+var _ = require('underscore');
 
 var app = express()
 	.use(express.compress())
 	.get('/locations', function(req,res,next){
-		res.json(locations);
+		var bbox = req.param('bbox').split(',');
+		bbox = _.map(bbox, function(m){return parseFloat(m);});
+		var filtered = _.filter(locations.features, function(f){
+			var lat = parseFloat(f.properties.latitude);
+			var lon = parseFloat(f.properties.longitude);
+			return bbox[1] <= lat && bbox[3] >= lat
+				&& bbox[0] <= lon && bbox[2] >= lon;
+		});
+		res.json({type : locations.type, features : filtered });
 	})
 	.use(express.static(__dirname + '/public'));
 
